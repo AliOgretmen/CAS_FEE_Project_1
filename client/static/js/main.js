ecfa1db1f36702af09818d8645fd72a1c3bd5687
AliOgretmen <aliogretmen.ch@gmail.com>
2018-06-27 01:28:50 +0200

    expanding buttons (description) problem is fixed

--- client/static/js/main.js
+++ client/static/js/main.js
@@ -1,87 +1,93 @@
-const showFinished = $('#showFinished');
-const sortByFinishedDate = $('#sortByFinishedDate');
-const sortByCreatedDate = $('#sortByCreatedDate');
-const sortByImportance = $('#sortByImportance');
-const isFinishedCheckbox = $(".is-finished");
-let toogleShowFinished = true;
-let interval = null;
+(function($, shared, helper, http){
 
-const renderTemplate = (filter = '', order = 'rate') => {
-    const theme = shared.myLocalStorage.getItem('theme');
-    shared.switchTheme(theme);
-    helper.setContainerId('main-container');
-    helper.setTemplateId('task-template');
-    helper.setFilterBy(filter);
-    helper.setOrderBy(order);
-/*
-    if (!interval) {
-        interval = setInterval(function () {
+    const showFinished = $('#showFinished');
+    const sortByFinishedDate = $('#sortByFinishedDate');
+    const sortByCreatedDate = $('#sortByCreatedDate');
+    const sortByImportance = $('#sortByImportance');
+    const isFinishedCheckbox = $(".is-finished");
+    let toggleFinishedNotes = true;
 
+    const renderTemplate = (filter = '', order = 'rate') => {
+        const theme = shared.myLocalStorage.getItem('theme');
+        shared.switchTheme(theme);
+        helper.setContainerId('main-container');
+        helper.setTemplateId('task-template');
+        helper.setFilterBy(filter);
+        helper.setOrderBy(order);
 
-        }, 3000);
-
-    }
-    */
-    storage.GetNotes(helper.orderBy, helper.filterBy, function (data) {
-        helper.setData(data);
-        helper.renderTodos();
-    })
-    shared.setCurrentDate();
-}
-
-sortByCreatedDate.on('click', () => {
-    renderTemplate('', 'due');
-});
-
-sortByFinishedDate.on('click', () => {
-    renderTemplate('', 'done');
-});
-sortByImportance.on('click', () => {
-    renderTemplate('', 'rate');
-});
-
-showFinished.on('click', () => {
-    toogleShowFinished = !toogleShowFinished;
-    if (toogleShowFinished) {
-        showFinished.html('Show Finished');
-        renderTemplate('', 'rate');
-    } else {
-        showFinished.html('All Tasks');
-        helper.setFilterBy('isFinished');
-        renderTemplate('isFinished', '');
-    }
-});
-
-const onFinishedClicked = (e) => {
-    const isChecked = $(e.target).is(":checked");
-    storage.GetNoteById(e.target.id, function (result) {
-        let selected = result.data;
-        selected.isFinished = isChecked;
-        selected.done = isChecked ? new Date() : '';
-        storage.UpdateNote(selected, function () {
-            renderTemplate();
+        http.getNotes(helper.orderBy, helper.filterBy, (data) => {
+            helper.setData(data);
+            helper.renderTodos();
+            checkDescriptions();
         });
+        shared.setCurrentDate();
+    }
+
+    sortByCreatedDate.on('click', () => {
+        renderTemplate('', 'due');
     });
-}
 
-const onToggleText = (target) => {
-    const elm = $(target).prev('.list-container');
-    if (elm.hasClass('expand')) {
-        elm.removeClass('expand');
-    } else {
-        elm.addClass('expand');
+    sortByFinishedDate.on('click', () => {
+        renderTemplate('', 'done');
+    });
+
+    sortByImportance.on('click', () => {
+        renderTemplate('', 'rate');
+    });
+
+    showFinished.on('click', () => {
+        toggleFinishedNotes = !toggleFinishedNotes;
+        if (toggleFinishedNotes) {
+            showFinished.html('Show Finished');
+            renderTemplate('', 'rate');
+        } else {
+            showFinished.html('All Tasks');
+            helper.setFilterBy('isFinished');
+            renderTemplate('isFinished', '');
+        }
+    });
+
+    const onFinishedClicked = (e) => {
+        const isChecked = $(e.target).is(":checked");
+        http.findNoteById(e.target.id, function (result) {
+            let selected = result.data;
+            selected.isFinished = isChecked;
+            selected.done = isChecked ? new Date() : '';
+            http.updateNote(selected, function () {
+                renderTemplate();
+            });
+        });
     }
-}
 
-document.getElementById('main-container').addEventListener('click', (e) => {
-    if (e.target.closest("button") && (e.target.closest("button").id === 'toggleText')) {
-        onToggleText(e.target.closest("button"));
-    } else if (e.target.classList.contains('is-finished')) {
-        onFinishedClicked(e);
+    const onToggleText = (target) => {
+        const elm = $(target).prev('.list-container');
+        if (elm.hasClass('expand')) {
+            elm.removeClass('expand');
+        } else {
+            elm.addClass('expand');
+        }
     }
-})
 
-renderTemplate();
+    $('#main-container').on('click', (e) => {
+        if ($(e.target).closest("button") && ($(e.target).closest("button").attr("id") === 'toggleText')) {
+            onToggleText($(e.target).closest("button"));
+        } else if ($(e.target).hasClass('is-finished')) {
+            onFinishedClicked(e);
+        }
+    });
+
+    function checkDescriptions(){
+        $('.list-container').each( function() {
+            if( $(this).html().match(/\n/gusm).length < 3){
+                $(this).next().hide();
+            }
+        })
+    }
+
+
+    renderTemplate();
+
+})(jQuery, shared, helper, http);
 
 
 
